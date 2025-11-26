e6dffee8d6372d0286a853c0af47e4d36c63b0e1
Daniel Draper <daniel.draper@octomind.dev>
2025-11-26 10:37:27 +0100

    fix eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends("plugin:mdx/recommended"),
+    plugins: {
+      prettier,
+    },
+    settings: {
+      "mdx/code-blocks": true,
+    },
+    ignores: ["posthog.js"],
+    rules: {
+      "prettier/prettier": "warn",
+      "no-unused-expressions": "off",
+    },
+  },
+]);
